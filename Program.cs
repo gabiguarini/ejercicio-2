8545e8934173073d22d11be840093d301eacbab6
Gabi Guarini <gabibea97@gmail.com>
2020-01-30 21:36:28 -0300

    Update

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nro1, nro2, result;
+            Console.WriteLine("ingrese 1 número");
+            nro1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro número");
+            nro2=int.Parse(Console.ReadLine());
+
+            result = nro1 * nro2;
+            Console.WriteLine("el producto es: " + result);
+            
+        }
+
+    }
+}
